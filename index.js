9f3ca2e1e699ba855f5406e76572ad09be80123c
Rina Hong <rhong1006@gmail.com>
2018-08-16 22:45:59 -0700

    Refactor index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const root = require('./routes');
 
-const root = require("./routes");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(root);
 
+// handle errors
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+})
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`🖥 Server listenning on PORT: ${PORT}`);
+});
