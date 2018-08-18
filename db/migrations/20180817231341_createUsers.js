2d986b5b30be05ac67f9623269616888a441a755
Rina Hong <rhong1006@gmail.com>
2018-08-18 01:23:47 -0700

    Add user table and register, login api works

--- db/migrations/20180817231341_createUsers.js
+++ db/migrations/20180817231341_createUsers.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
-    table.increments('id')
-    table.string('email').unique().notNull()
-    table.string('username').unique().notNull()
-    table.string('passwordDigest').notNull()
-    table.timestamps(false, true)
+    table.increments('id');
+    table.string('email').unique().notNull();
+    table.string('username').unique().notNull();
+    table.string('passwordDigest').notNull();
+    table.timestamps(false, true);
   });
 };
 
